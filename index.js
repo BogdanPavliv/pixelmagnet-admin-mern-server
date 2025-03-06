c8dad27d858a0543dc3e723b24abe397f384c0eb
Bogdan <bogdan.pavliv@gmail.com>
2025-03-06 19:58:22 +0200

    add project

--- index.js
+++ index.js
@@ -32,9 +32,10 @@ app.use(express.json())
 app.use(express.static('uploads'))
 
 // app.use((req, res, next) => {
-//     console.log(`Incoming request: ${req.method} ${req.url}`, req.body);
+//     console.log(`Incoming request: ${req.method} ${req.url}`);
 //     next();
 // });
+
 
 // Routes
 // http://localhost:3002
